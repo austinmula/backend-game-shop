be32b3621e90223989f1541b5e338c1236900da3
cymula <amula@cytonn.com>
2023-01-31 12:16:26 +0300

    added serach by category

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -28,29 +28,77 @@ exports.getGameDetails = async (req, res) => {
 };
 
 exports.getAllGames = async (req, res) => {
-  let query = "SELECT * from games";
-  await con.query(query, [req.params.id], (error, response) => {
-    if (error) return sendError(res, error, 400);
+  try {
+    let query = "SELECT * from games";
+    await con.query(query, (error, response) => {
+      if (error) return sendError(res, error, 400);
 
-    res.send(response);
-  });
+      res.send(response);
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+    console.log(error.message);
+  }
+};
+
+exports.getGamesByCategory = (req, res) => {
+  const category = req.params.id;
+  console.log(category);
+  try {
+    let query = "SELECT * from all_categories WHERE category LIKE ?";
+    con.query(query, ["%" + category + "%"], (error, response) => {
+      if (error) return sendError(res, error, 400);
+
+      res.send(response);
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+    console.log(error.message);
+  }
+};
+
+exports.getGamesByConsole = (req, res) => {
+  const platf = req.params.id;
+  console.log(platf);
+  try {
+    let query = "SELECT * from all_consoles WHERE console LIKE ?";
+    con.query(query, ["%" + platf + "%"], (error, response) => {
+      if (error) return sendError(res, error, 400);
+
+      res.send(response);
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+    console.log(error.message);
+  }
 };
 
 exports.createGame = async (req, res) => {
-  const { name, description, image, price, publisher, category, consoles } =
-    req.body;
+  let new_id;
+  const {
+    name,
+    description,
+    image,
+    price,
+    publisher,
+    category,
+    consoles,
+    rating,
+  } = req.body;
 
   try {
     let query =
-      "INSERT INTO games (name, description, image, price, publisher) VALUES (?, ?, ?, ?, ?)";
+      "INSERT INTO games (name, description, image, price, publisher, rating) VALUES (?, ?, ?, ?, ?, ?)";
     await con.query(
       query,
-      [name, description, image, price, publisher],
+      [name, description, image, price, publisher, rating],
       async (error, response) => {
         if (error) return sendError(res, error, 400);
 
         const game_category = mapItemToId(response.insertId, category);
         const game_console = mapItemToId(response.insertId, consoles);
+
+        new_id = response.insertId;
 
         await con.query(
           "INSERT INTO game_category (game_id, category_id) VALUES ?",
@@ -68,7 +116,15 @@ exports.createGame = async (req, res) => {
           }
         );
 
-        res.send("successfuly Added");
+        con.query(
+          "SELECT * FROM games where id = ?",
+          [new_id],
+          (error, response) => {
+            if (error) return sendError(res, error, 400);
+
+            res.send(response[0]);
+          }
+        );
       }
     );
   } catch (error) {
--- routes/games.routes.js
+++ routes/games.routes.js
@@ -3,9 +3,15 @@ const router = require("express").Router();
 const {
   createGame,
   getGameDetails,
+  getAllGames,
+  getGamesByCategory,
+  getGamesByConsole,
 } = require("../controllers/games.controller");
 
 router.post("/add", createGame);
 router.get("/:id", getGameDetails);
+router.get("/", getAllGames);
+router.get("/category/:id", getGamesByCategory);
+router.get("/console/:id", getGamesByConsole);
 
 module.exports = router;
